65e4d479f7efb352d358e87ce82cd48725b588da
J0SAN <jose22ekprogo@gmail.com>
2026-04-26 18:22:12 -0500

    Estructura base MVVM y directorios del proyecto

---
+++ app/src/main/java/com/proyecto/kuskahaku/presentation/theme/ui/Color.kt
@@ -0,0 +1,12 @@
+package com.proyecto.kuskahaku.presentation.theme.ui
+
+import androidx.compose.ui.graphics.Color
+
+val ChichaPurple = Color(0xFF5D327C)
+val CocaGreen = Color(0xFF386641)
+val IntiGold = Color(0xFFF2CC8F)
+val EarthRed = Color(0xFFBC4749)
+val StoneWhite = Color(0xFFF4F1DE)
+
+val ChichaPurpleLight = Color(0xFF9E7BB5)
+val StoneDark = Color(0xFF1B1B1B)
---
+++ app/src/main/java/com/proyecto/kuskahaku/presentation/theme/ui/Theme.kt
@@ -0,0 +1,52 @@
+package com.proyecto.kuskahaku.presentation.theme.ui
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorScheme = darkColorScheme(
+    primary = ChichaPurpleLight,
+    secondary = CocaGreen,
+    tertiary = IntiGold,
+    background = StoneDark,
+    surface = StoneDark
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = ChichaPurple,
+    secondary = CocaGreen,
+    tertiary = IntiGold,
+    background = StoneWhite,
+    surface = Color.White,
+    onBackground = Color (0xFF2B2D42),
+    onSurface = Color(0xFF2B2D42)
+
+    /* Other default colors to override
+    background = Color(0xFFFFFBFE),
+    surface = Color(0xFFFFFBFE),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F),
+    */
+)
+
+@Composable
+fun KuskaHakuTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
